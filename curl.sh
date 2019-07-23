625994921733425b236c8f97dcd0ead0da65f8c2
Nurlan199206 <user_1771@mail.ru>
2019-07-23 09:48:03 +0600

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 up=200
 down=000
-test=$(/usr/bin/curl --write-out "%{http_code}\n" --silent --output /dev/null https://sysadm.kz)
+test=$(/usr/bin/curl --write-out "%{http_code}\n" --silent --output /dev/null https://website.kz)
 if [ "$test" = "$up" ]
 then
 echo "it's works!"
